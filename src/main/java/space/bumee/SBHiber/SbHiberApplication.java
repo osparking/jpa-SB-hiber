cc8acf175e32e0f93b0ef8ad1c79a94877b19082
osparking <jbpark03@gmail.com>
2024-06-28 21:06:11 +0900

    Spring boot 와 hibernate - JSP

---
+++ src/main/java/space/bumee/SBHiber/SbHiberApplication.java
@@ -0,0 +1,13 @@
+package space.bumee.SBHiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHiberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumee/SBHiber/SbHiberApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumee.SBHiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

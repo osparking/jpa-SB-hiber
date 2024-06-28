ad62e58d136c665baadac0f01382c743abf15995
osparking <jbpark03@gmail.com>
2024-06-29 07:53:37 +0900

    Book 엔티티 클래스

---
+++ src/main/java/space/bumee/SBHiber/models/Book.java
@@ -0,0 +1,16 @@
+package space.bumee.SBHiber.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+
+}

6d9797a1df93df157bc43a4998fdbf0f7812ce77
osparking <jbpark03@gmail.com>
2024-06-30 15:39:30 +0900

    소유측에 외래키 employee_id 지정

--- src/main/java/space/bumee/SBHiber/models/Email.java
+++ src/main/java/space/bumee/SBHiber/models/Email.java
@@ -2,6 +2,8 @@ package space.bumee.SBHiber.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -9,10 +11,11 @@ import jakarta.persistence.ManyToOne;
 @Entity
 public class Email {
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-  
+
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "manager_id")
+  @JoinColumn(name = "employee_id")
   private Manager manager;
 
 }

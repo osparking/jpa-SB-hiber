c8a1e8fca5611b9ff8c16ebe5277380035d77ede
osparking <jbpark03@gmail.com>
2024-06-30 16:14:24 +0900

    양방향 관계 형성 - 직원 객체에 이메일 목록

--- src/main/java/space/bumee/SBHiber/models/Employee.java
+++ src/main/java/space/bumee/SBHiber/models/Employee.java
@@ -1,13 +1,17 @@
 package space.bumee.SBHiber.models;
 
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -20,6 +24,9 @@ public class Employee {
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "id")
   private Long id;
+  
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
+  private List<Email> emails;
   
   private String ename;
 

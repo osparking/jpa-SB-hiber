9411facdf4ae2a207bc85c9f63786fea60f7794c
osparking <jbpark03@gmail.com>
2024-06-30 11:18:24 +0900

    모든 엔티티 클래스에 Id 필드 추가

--- src/main/java/space/bumee/SBHiber/models/Email.java
+++ src/main/java/space/bumee/SBHiber/models/Email.java
@@ -2,11 +2,15 @@ package space.bumee.SBHiber.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Email {
+  @Id
+  private Long id;
+  
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "manager_id")
   private Manager manager;
--- src/main/java/space/bumee/SBHiber/models/Office.java
+++ src/main/java/space/bumee/SBHiber/models/Office.java
@@ -2,11 +2,15 @@ package space.bumee.SBHiber.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 
 @Entity
 public class Office {
+  @Id
+  private Long id;
+  
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "addressId")
   private Address address;

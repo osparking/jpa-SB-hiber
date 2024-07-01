d37a5b83f3acfd0714cc2e85ecf6defda1c98d59
osparking <jbpark03@gmail.com>
2024-07-02 08:54:46 +0900

    주키를 DBMS 에서 자동 생산하게 설정

--- src/main/java/space/bumee/SBHiber/entities/Student.java
+++ src/main/java/space/bumee/SBHiber/entities/Student.java
@@ -1,6 +1,8 @@
 package space.bumee.SBHiber.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,9 @@ import lombok.Data;
 @Data
 public class Student {
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long studentId;
+  
+  private String name;
 
 }

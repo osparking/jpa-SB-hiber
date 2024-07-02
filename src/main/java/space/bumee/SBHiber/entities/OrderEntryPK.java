ba518e35748a5b1f4d365f617b4c8deef610a149
osparking <jbpark03@gmail.com>
2024-07-02 16:39:12 +0900

    many to one 필드 in embeddable 클래스 주키

--- src/main/java/space/bumee/SBHiber/entities/OrderEntryPK.java
+++ src/main/java/space/bumee/SBHiber/entities/OrderEntryPK.java
@@ -3,6 +3,7 @@ package space.bumee.SBHiber.entities;
 import java.io.Serializable;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -16,4 +17,6 @@ public class OrderEntryPK implements Serializable {
   private static final long serialVersionUID = 1L;
   private long orderId;
   private long productId;
+  @ManyToOne
+  private Person person;
 }

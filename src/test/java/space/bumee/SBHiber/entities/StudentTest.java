006ceb31be4e0edf19a352adc686104f421403a9
osparking <jbpark03@gmail.com>
2024-07-02 10:39:03 +0900

    학생 ID 값 증가 - 시퀀스 자동

--- src/test/java/space/bumee/SBHiber/entities/StudentTest.java
+++ src/test/java/space/bumee/SBHiber/entities/StudentTest.java
@@ -1,5 +1,6 @@
 package space.bumee.SBHiber.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,10 @@ class StudentTest {
     var st = new Student();
     st.setName("이");
     st = studentRepo.save(st);
-    assertNotNull(st.getStudentId());
+    var st2 = new Student();
+    st2.setName("김");
+    st2 = studentRepo.save(st2);
+    var stList = studentRepo.findAll();
+    assertEquals(2, stList.size());
   }
 }

daf643d2ada2721cc23ea364a93d77f222f3b485
osparking <jbpark03@gmail.com>
2024-07-02 20:42:20 +0900

    주키인 id 가 다른 개체에 의해 공유됨

---
+++ src/main/java/space/bumee/SBHiber/entities/StoreUser.java
@@ -0,0 +1,21 @@
+package space.bumee.SBHiber.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class StoreUser {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String name;
+
+}
